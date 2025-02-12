1e5013059f739e3d0f7124f632a2cf6c5faf7e65
makumar21 <makumar@ex2india.com>
2025-02-12 15:38:57 +0530

    index and socket js file updated in backend

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat.ex2india.dev:5173"],
+    origin: ["http://localhost:5173"],
   },
 });
 
